374fd6101eee53347cab56076eb64cfb863707e5
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-06-04 17:47:37 +0200

    Sortowanie kubełkowe

--- AiSD/Sortowanie/bucketsort.c
+++ AiSD/Sortowanie/bucketsort.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct bucketsort_el_listy
+struct el_listy
 {
-    struct bucketsort_el_listy *nast;
-    int wart;
+    struct el_listy *next;
+    int value;
 };
 
-struct bucketsort_el_listy *bucketsort(int *tab, int n)
+struct el_listy *bucketsort(int *tab, int n)
 {
-    struct bucketsort_el_listy *poczatek[100];
+    struct el_listy *poczatek[100];
     for (int i = 0; i < 100; i++)
     {
         poczatek[i] = 0;
     }
 
-    struct bucketsort_el_listy *w;
+    struct el_listy *w;
 
     for (int i = 0; i < 20; i++)
     {
-        w = malloc(sizeof(struct bucketsort_el_listy));
-        w->wart = tab[i];
-        w->nast = 0;
+        w = malloc(sizeof(struct el_listy));
+        w->value = tab[i];
+        w->next = 0;
 
         if (poczatek[tab[i]] == 0)
         {
@@ -29,13 +29,13 @@ struct bucketsort_el_listy *bucketsort(int *tab, int n)
         }
         else
         {
-            w->nast = poczatek[tab[i]];
+            w->next = poczatek[tab[i]];
             poczatek[tab[i]] = w;
         }
     }
 
     int i = 0;
-    struct bucketsort_el_listy *ws0;
+    struct el_listy *ws0;
     for (; poczatek[i] == 0; i++)
         ;
 
@@ -47,11 +47,11 @@ struct bucketsort_el_listy *bucketsort(int *tab, int n)
     {
         if (poczatek[i] != 0)
         {
-            while (w->nast != 0)
+            while (w->next != 0)
             {
-                w = w->nast;
+                w = w->next;
             }
-            w->nast = poczatek[i];
+            w->next = poczatek[i];
         }
     }
 
@@ -63,14 +63,14 @@ int main(void)
     int tablica[20] = {1, 5, 4, 7, 12, 17, 11, 8, 0, 86,
                        12, 1, 17, 2, 21, 56, 53, 20, 10, 15};
 
-    struct bucketsort_el_listy *ws0;
+    struct el_listy *ws0;
 
     ws0 = bucketsort(tablica, 20);
 
     printf("Bucket sort\n");
-    for (struct bucketsort_el_listy *w = ws0; w != 0; w = w->nast)
+    for (struct el_listy *w = ws0; w != 0; w = w->next)
     {
-        printf("%d ", w->wart);
+        printf("%d ", w->value);
     }
     printf("\n");
 }
---
+++ AiSD/Sortowanie/bucketsort_advanced.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+struct el_listy
+{
+    int next;
+    int value;
+};
+
+int sort(const int *tab, int n, int liczba_kubelkow, struct el_listy *element)
+{
+    int poczatek_1[liczba_kubelkow + 1];
+    int koniec_1[liczba_kubelkow + 1];
+    struct el_listy *ws = element;
+    int ws0, ws2;
+
+    for (int i = 0; i < liczba_kubelkow + 1; i++)
+    {
+        poczatek_1[i] = -1;
+        koniec_1[i] = -1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        ws->value = tab[i];
+        ws->next = -1;
+
+        if (poczatek_1[ws->value] == -1)
+        {
+            poczatek_1[ws->value] = i;
+            koniec_1[ws->value] = i;
+        }
+
+        else
+        {
+            element[koniec_1[ws->value]].next = i;
+            koniec_1[ws->value] = i;
+        }
+        ws++;
+    }
+
+    int i = 0;
+    for (i = 0; poczatek_1[i] == -1; i++)
+        ;
+    ws0 = poczatek_1[i];
+    ws2 = koniec_1[i];
+    for (i++; i <= liczba_kubelkow; i++)
+    {
+        if (poczatek_1[i] != -1)
+        {
+            element[ws2].next = poczatek_1[i];
+            ws2 = koniec_1[i];
+        }
+    }
+
+    return ws0;
+}
+
+int main(void)
+{
+    int tablica[20] = {1, 5, 4, 7, 12, 17, 11, 8, 0, 86,
+                       12, 1, 17, 2, 21, 56, 53, 20, 10, 15};
+    struct el_listy wynik[20];
+    
+    int w = sort(tablica, 20, 100, wynik);
+
+    printf("Bucketsort - advanced\n");
+    for (; w != -1; w = wynik[w].next)
+    {
+        printf("%d ", wynik[w].value);
+    }
+    printf("\n");
+}
